9e7388251937262dc8455f61605b1b323ea0d3e2
Israel Canessa <israellcanessa@gmail.com>
2020-05-02 00:16:05 +0000

    Automatically backed up by Learn

--- spec/hello_world_spec.rb
+++ spec/hello_world_spec.rb
@@ -8,3 +8,4 @@ describe "Hello World" do
     # we are testing this, focus on simply coding your solution.
     expect($stdout).to receive(:puts).with("Hello World!")
     load './hello_world.rb'
+  end
